e18a87e35d5643e94b7eb70882d7dffa46c43760
katanujoy <katanujoyy99@gmail.com>
2025-03-05 10:54:03 +0300

    Completed JavaScript Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to Flatbook.`;
+}
+
+function excitedGreeting(name) {
+    return `HELLO, ${name.toUpperCase()}! WELCOME TO FLATBOOK!`;
+}
+
+function shortGreeting(name) {
+    return `Hi, ${name.slice(0, 1)}!`;
+}
+console.log(greetUser("Alice")); 
+console.log(excitedGreeting("Alice")); 
+console.log(shortGreeting("Alice")); 
